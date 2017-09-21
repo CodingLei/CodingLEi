02d173a134efcb4b659febf35462cc748c700405
Administrator <Administrator@WIN7-20150630TU>
2017-09-21 15:14:09 +0800

    添加项目文件。ceshi

---
+++ CodingLei_Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodingLei_Admin.MvcApplication" Language="C#" %>
---
+++ CodingLei_Model/SearchModel/DemoSearchModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodingLei_Model.SearchModel;
+
+namespace CodingLei_Model.SearchModel
+{
+    public class DemoSearchModel : BaseSearchModel
+    {
+        public string BrandName { get; set; }
+    }
+}
