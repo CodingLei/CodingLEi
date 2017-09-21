16907cc35e338d3c57e62bfc50e86c7cd20c4372
Administrator <Administrator@WIN7-20150630TU>
2017-09-21 15:14:09 +0800

    添加项目文件。

---
+++ CodingLei_Admin/Controllers/Account/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodingLei_Business.Business;
+
+
+namespace CodingLei_Admin.Controllers.Account
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult LoginSubmit()
+        {
+            string username = Request["UserName"];
+            string password = Request["PassWord"];
+            return Json(new LoginBusiness().LoginSubmit(username, password));
+        }
+    }
+}
---
+++ CodingLei_Business/Business/LoginBusiness.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using  CodingLei_Model.ExtendModel;
+using System.Web;
+using CodingLei_Common.Helper;
+using CodingLei_Model.TableModel;
+
+namespace CodingLei_Business.Business
+{
+    public class LoginBusiness
+    {
+        public ReturnInfoModel LoginSubmit(string username, string password)
+        {
+            var employeeModel = DBHelper.Context.From<Employee>().Where(d => d.Phone == username && d.LoginPassword == password).ToList();
+            if (employeeModel == null || employeeModel.Count <= 0)
+            {
+                return ReturnInfo.Failure("用户名或密码不正确！");
+            }
+
+            HttpCookie userCookie = HttpContext.Current.Response.Cookies["LoginInfo"];
+            if (userCookie != null)
+            {
+                HttpContext.Current.Response.Cookies.Remove("LoginInfo");
+            }
+            userCookie = new HttpCookie("LoginInfo");
+            userCookie.Values["UserName"] = username;
+            userCookie.Values["Password"] = password;
+            userCookie.Expires = DateTime.Now.AddDays(1);
+            HttpContext.Current.Response.Cookies.Add(userCookie);
+            return ReturnInfo.Success();
+        }
+    }
+}
